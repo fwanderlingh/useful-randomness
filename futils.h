214f3cd2fc798c7a5ddf509a31bdc40bc9dd0168
merosss <fwanderlingh@gmail.com>
2015-07-29 15:26:02 +0100

    first commit

---
+++ futils.h
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+
+namespace FUTILS
+{
+struct Spinner {
+/** 
+  * Prints a spinner that, put in a while loop with negligible execution time,
+  * spins at a given frequency.
+ */
+	Spinner(int freq) : freq(freq), spinIndex(0), spin_chars("/-\\|") {
+		clock_gettime(CLOCK_MONOTONIC, &last);
+		period = 1/static_cast<double>(freq+1E-6);
+		//std::cout << "period: " << period << "s" << std::endl;
+	}
+	
+	void operator()(void) {
+	
+		clock_gettime(CLOCK_MONOTONIC, &now);
+	  double timeElapsed = ( now.tv_sec - last.tv_sec )
+             + ( now.tv_nsec - last.tv_nsec ) / 1E9;
+            
+    //std::cout << "timeElapsed: " << timeElapsed << std::endl;
+		if(period - timeElapsed < 1E-3){  
+		  //std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
+			putchar(' ');
+		  putchar(spin_chars[spinIndex % spin_chars.length()]);
+		  putchar(' ');
+		  fflush(stdout);
+		  putchar('\r');
+		  spinIndex++;
+		  last = now;
+    }
+    
+  }
+	
+	private:
+		struct timespec last, now;
+		int freq;
+		double period;
+		unsigned long spinIndex;
+		std::string spin_chars;
+};
+
+struct Dotter {
+/** 
+  * Prints a classic dot animation that, put in a while loop with negligible execution time,
+  * plays at a given frequency.
+ */
+	Dotter(int freq) : freq(freq), spinIndex(0), spin_chars("... .. .. .. .... .... .") {
+		clock_gettime(CLOCK_MONOTONIC, &last);
+		period = 1/static_cast<double>(freq);
+		//std::cout << "period: " << period << "s" << std::endl;
+	}
+	
+	void operator()(void) {
+	
+		clock_gettime(CLOCK_MONOTONIC, &now);
+	  double timeElapsed = ( now.tv_sec - last.tv_sec )
+             + ( now.tv_nsec - last.tv_nsec ) / 1E9;
+            
+    //std::cout << "timeElapsed: " << timeElapsed << std::endl;
+		if(period - timeElapsed < 1E-3){  
+		  //std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
+			putchar(' ');
+		  putchar(spin_chars[spinIndex % spin_chars.length()]);
+		  putchar(spin_chars[(spinIndex % spin_chars.length())+1]);
+ 			putchar(spin_chars[(spinIndex % spin_chars.length())+2]);
+  		putchar(spin_chars[(spinIndex % spin_chars.length())+3]);
+		  putchar(' ');
+		  fflush(stdout);
+		  putchar('\r');
+		  spinIndex+=4;
+		  last = now;
+    }
+    
+  }
+	
+	private:
+		struct timespec last, now;
+		int freq;
+		double period;
+		unsigned long spinIndex;
+		std::string spin_chars;
+};
+}
