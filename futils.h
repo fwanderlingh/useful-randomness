0a4ec055bbd485f48d370774f2ad1b9cbf6f488e
merosss <fwanderlingh@gmail.com>
2015-10-07 14:52:42 +0100

    [FUTILS] Added VectorPrint functions

--- futils.h
+++ futils.h
@@ -2,88 +2,108 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
+namespace FUTILS {
+/// My first functor!!! :D
 
-namespace FUTILS
+/** 
+ * Prints a spinner that, put in a while loop with negligible execution time,
+ * spins at a given frequency.
+ */
+struct Spinner
 {
-struct Spinner {
-/** 
-  * Prints a spinner that, put in a while loop with negligible execution time,
-  * spins at a given frequency.
- */
-	Spinner(int freq) : freq(freq), spinIndex(0), spin_chars("/-\\|") {
+	Spinner(int frequency) :
+			freq(frequency), spinIndex(0), spin_chars("/-\\|") {
 		clock_gettime(CLOCK_MONOTONIC, &last);
-		period = 1/static_cast<double>(freq+1E-6);
+		period = 1 / static_cast<double>(freq + 1E-6);
 		//std::cout << "period: " << period << "s" << std::endl;
 	}
-	
+
 	void operator()(void) {
-	
+
 		clock_gettime(CLOCK_MONOTONIC, &now);
-	  double timeElapsed = ( now.tv_sec - last.tv_sec )
-             + ( now.tv_nsec - last.tv_nsec ) / 1E9;
-            
-    //std::cout << "timeElapsed: " << timeElapsed << std::endl;
-		if(period - timeElapsed < 1E-3){  
-		  //std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
+		double timeElapsed = (now.tv_sec - last.tv_sec) + (now.tv_nsec - last.tv_nsec) / 1E9;
+
+		//std::cout << "timeElapsed: " << timeElapsed << std::endl;
+		if (period - timeElapsed < 1E-3) {
+			//std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
 			putchar(' ');
-		  putchar(spin_chars[spinIndex % spin_chars.length()]);
-		  putchar(' ');
-		  fflush(stdout);
-		  putchar('\r');
-		  spinIndex++;
-		  last = now;
-    }
-    
-  }
-	
-	private:
-		struct timespec last, now;
-		int freq;
-		double period;
-		unsigned long spinIndex;
-		std::string spin_chars;
+			putchar(spin_chars[spinIndex % spin_chars.length()]);
+			putchar(' ');
+			fflush(stdout);
+			putchar('\r');
+			spinIndex++;
+			last = now;
+		}
+
+	}
+
+private:
+	struct timespec last, now;
+	int freq;
+	double period;
+	unsigned long spinIndex;
+	std::string spin_chars;
 };
 
-struct Dotter {
-/** 
-  * Prints a classic dot animation that, put in a while loop with negligible execution time,
-  * plays at a given frequency.
- */
-	Dotter(int freq) : freq(freq), spinIndex(0), spin_chars("... .. .. .. .... .... .") {
+struct Dotter
+{
+	/**
+	 * Prints a classic dot animation that, put in a while loop with negligible execution time,
+	 * plays at a given frequency.
+	 */
+	Dotter(int freq) :
+			freq(freq), spinIndex(0), spin_chars("... .. .. .. .... .... .") {
 		clock_gettime(CLOCK_MONOTONIC, &last);
-		period = 1/static_cast<double>(freq);
+		period = 1 / static_cast<double>(freq);
 		//std::cout << "period: " << period << "s" << std::endl;
 	}
-	
+
 	void operator()(void) {
-	
+
 		clock_gettime(CLOCK_MONOTONIC, &now);
-	  double timeElapsed = ( now.tv_sec - last.tv_sec )
-             + ( now.tv_nsec - last.tv_nsec ) / 1E9;
-            
-    //std::cout << "timeElapsed: " << timeElapsed << std::endl;
-		if(period - timeElapsed < 1E-3){  
-		  //std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
+		double timeElapsed = (now.tv_sec - last.tv_sec) + (now.tv_nsec - last.tv_nsec) / 1E9;
+
+		//std::cout << "timeElapsed: " << timeElapsed << std::endl;
+		if (period - timeElapsed < 1E-3) {
+			//std::cout << "fabs(freq - timeElapsed): " << fabs(freq - timeElapsed) << std::endl;
 			putchar(' ');
-		  putchar(spin_chars[spinIndex % spin_chars.length()]);
-		  putchar(spin_chars[(spinIndex % spin_chars.length())+1]);
- 			putchar(spin_chars[(spinIndex % spin_chars.length())+2]);
-  		putchar(spin_chars[(spinIndex % spin_chars.length())+3]);
-		  putchar(' ');
-		  fflush(stdout);
-		  putchar('\r');
-		  spinIndex+=4;
-		  last = now;
-    }
-    
-  }
-	
-	private:
-		struct timespec last, now;
-		int freq;
-		double period;
-		unsigned long spinIndex;
-		std::string spin_chars;
+			putchar(spin_chars[spinIndex % spin_chars.length()]);
+			putchar(spin_chars[(spinIndex % spin_chars.length()) + 1]);
+			putchar(spin_chars[(spinIndex % spin_chars.length()) + 2]);
+			putchar(spin_chars[(spinIndex % spin_chars.length()) + 3]);
+			putchar(' ');
+			fflush(stdout);
+			putchar('\r');
+			spinIndex += 4;
+			last = now;
+		}
+
+	}
+
+private:
+	struct timespec last, now;
+	int freq;
+	double period;
+	unsigned long spinIndex;
+	std::string spin_chars;
 };
+
+template<typename T>
+void PrintSTLVector(T vecObj) {
+	for (typename T::iterator itr = vecObj.begin(); itr != vecObj.end(); ++itr) {
+		std::cout << *itr << " ";
+	}
+}
+
+template<typename T>
+void PrintSTLVectOfVects(T vecObj) {
+	for (typename T::iterator itr = vecObj.begin(); itr != vecObj.end(); ++itr) {
+		std::cout << "#" << itr - vecObj.begin() << ": ";
+		PrintSTLVector(*itr);
+		std::cout << "\n";
+	}
+}
+
 }
